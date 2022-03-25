3566681dd4937e5eb4d9c63b3a620770f0c7f7f0
Sevrinn <sevrinnsevrinn@gmail.com>
2022-03-25 08:09:59 -0700

    show player turn

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -86,3 +86,5 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
